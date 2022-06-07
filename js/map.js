22fcfa60370b32765dd305f6cfa2e90e46704b61
Gusbod <skrapiskrap@gmail.com>
2022-06-07 18:49:51 +0200

    Updated data

--- js/map.js
+++ js/map.js
@@ -172,28 +172,15 @@ function matchClusterData(feature, layer)
     // console.log(feature.properties.name);
 }
 
-// fetch('./data/campclusters.geojson').then(response => response.json()).then(response => 
-// {
-//     L.geoJson(response, {style: getStyle(response.name), onEachFeature: matchClusterData} ).addTo(map)
-//     .eachLayer(function (layer) {layer.bindPopup(layer.feature.properties.name)
-//     //  .bindTooltip(layer.feature.properties.name);
-// });
-// });
-
 //loop through geojsonfiles, use fetch to download them and add as a layer to the map
 for (var i = 0; i < geojsonFiles.length; i++) 
 {
     fetch(geojsonFiles[i]).then(response => response.json()).then(response => 
     {
         L.geoJson(response, {style: getStyle(response.name), onEachFeature: onEachFeature} ).addTo(map)
-        .eachLayer(function (layer) {layer.bindPopup(layer.feature.properties.name)
-        //  .bindTooltip(layer.feature.properties.name);
-	});
+        .eachLayer(function (layer) {layer.bindPopup(layer.feature.properties.name)});
     });
 }
-
-
-
 
 
     // var options = { 
@@ -211,7 +198,6 @@ for (var i = 0; i < geojsonFiles.length; i++)
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 20,
     attribution: 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    // id: 'mapbox/streets-v11',
     id: 'mapbox/satellite-v9',
     tileSize: 512,
     zoomOffset: -1
