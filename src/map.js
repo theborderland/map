0503d11ebe5666d5300f9199c4126d1b531be4cd
Gusbod <skrapiskrap@gmail.com>
2022-06-13 01:30:08 +0200

    Added slopemap and updated layer control

--- src/map.js
+++ src/map.js
@@ -4,6 +4,7 @@ import 'leaflet.polylinemeasure';
 import { loadZones } from './loaders/loadZones';
 import { loadNatureReserve } from './loaders/loadNatureReserve';
 import { loadSoundGuide } from './loaders/loadSoundGuide';
+import { loadSlopeMap } from './loaders/loadSlopeMap';
 import { loadFireRoads } from './loaders/loadFireRoads';
 import { loadCampClusters } from './loaders/loadCampClusters';
 import { loadTooltipZoom, loadBoarderlandMarker } from './utils/loadTooltipZoom';
@@ -14,17 +15,21 @@ export const createMap = async () => {
     const map = L.map('map', { zoomControl: false, maxZoom: 21 }).setView([57.621111, 14.927857], 17);
 
 
-    await loadZones(map);
-    await loadNatureReserve(map);
-    await loadSoundGuide(map);
-    await loadFireRoads(map);
-    await loadCampClusters(map);
-    await loadTooltipZoom(map);
-    await loadBoarderlandMarker(map);
-    await loadCampMarkers(map);
-    await loadPositionControl(map);
+    loadZones(map);
+    loadNatureReserve(map);
+    loadFireRoads(map);
+    loadCampClusters(map);
+    loadTooltipZoom(map);
+    loadBoarderlandMarker(map);
+    loadCampMarkers(map);
+    loadPositionControl(map);
+    let sound = await loadSoundGuide(map);
+    let slope = await loadSlopeMap(map);
     L.control.scale({metric: true, imperial: false}).addTo(map);
     L.control.polylineMeasure().addTo(map);
+
+    var baseLayers = {"Sound guide": sound, "Slope": slope};
+    L.control.layers(null, baseLayers).addTo(map);
 
     const googleSatellite = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
         maxZoom: 20,
