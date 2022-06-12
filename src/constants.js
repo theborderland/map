2b210128170d200dc30ac87f673cf763203450d2
Tim Gremalmm <tim@gremalm.se>
2022-06-12 06:19:22 +0200

    Load camps in feature properties and render

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,5 @@ export const CAMP_CLUSTERS_SPREADSHEET_JSON =
     'https://sheets.googleapis.com/v4/spreadsheets/1HBERVykwMRDlTGQpOJHmoeDSLVcGriB9V4-FU_F8iag/values/allclusters!A2:H150?alt=json&key=AIzaSyAwMjpopKPH4_7Kn8qNhrOGz4c-JBv3QG0';
 
 export const CAMP_CLUSTERS_GEOJSON = './data/areas.geojson';
+export const CAMP_CAMPS_SPREADSHEET_JSON =
+    'https://sheets.googleapis.com/v4/spreadsheets/1GUOHOdrUGk9SsBeE83Z1wadbmqqG-_OKN2VT2jKVB7A/values/Placement!A3:O800?alt=json&key=AIzaSyAwMjpopKPH4_7Kn8qNhrOGz4c-JBv3QG0';
--- src/loaders/loadCampClusters.js
+++ src/loaders/loadCampClusters.js
@@ -1,9 +1,186 @@
-import { CAMP_CLUSTERS_GEOJSON, CAMP_CLUSTERS_SPREADSHEET_JSON } from '../constants';
+import { CAMP_CAMPS_SPREADSHEET_JSON, CAMP_CLUSTERS_GEOJSON, CAMP_CLUSTERS_SPREADSHEET_JSON } from '../constants';
 import L from 'leaflet';
 import { loadSpreadSheet } from '../utils/loadSpreadSheet';
 import { loadGeoJson } from '../utils/loadGeoJson';
+import { Console } from 'console';
+
+const loadAreaCamps = async () => {
+    let areas = {};
+      
+    const sheetdata = await loadSpreadSheet(CAMP_CAMPS_SPREADSHEET_JSON);
+
+    let row_segment_type = "";
+    let row_segment_counter = 0;
+    let row_segment_name = "";
+
+    // Loop through sheetdata and group them by area
+    for (let i = 0; i < sheetdata.length; i++)
+    {
+        let row = sheetdata[i];
+        // Analyze non empty rows
+        if (row.length > 0)
+        {
+            // If first col is empty it's a continuation of previous area or zone
+            if (row[0] == '')
+            {
+                row_segment_counter += 1;
+            }
+            else
+            {
+                // It's a new area or zone
+                row_segment_counter = 0;
+                row_segment_type = row[0];
+            }
+            // Extract data
+            if (row_segment_type == 'camp' || row_segment_type == 'art' || row_segment_type == 'sound' || row_segment_type == 'building')
+            {
+                // Extract area information
+                if (row_segment_counter == 0)
+                {
+                    // Init cluster area
+                    row_segment_name = row[1];
+                    areas[row_segment_name] = {};
+                    areas[row_segment_name].name = row_segment_name;
+                    areas[row_segment_name].type = "";
+                    areas[row_segment_name].size = 0;
+                    areas[row_segment_name].size_reserved = 0;
+                    areas[row_segment_name].size_usage_percent = "";
+                    areas[row_segment_name].power_usage = 0;
+                    areas[row_segment_name].camps = {};
+                }
+                else if (row_segment_counter == 1)
+                {
+                    // Extract size
+                    if (parseInt(row[1]))
+                    {
+                        areas[row_segment_name].size = parseInt(row[1]);
+                    }
+                }
+                else if (row_segment_counter == 2)
+                {
+                    // Extract reserved size
+                    if (parseInt(row[1]))
+                    {
+                        areas[row_segment_name].size_reserved = parseInt(row[1]);
+                    }
+                }
+                else if (row_segment_counter == 3)
+                {
+                    // Extract ussage of area in percent
+                    if (parseInt(row[1]))
+                    {
+                        areas[row_segment_name].size_usage_percent = parseInt(row[1]);
+                    }
+                }
+                else if (row_segment_counter == 4)
+                {
+                    // Extract power usage
+                    if (parseInt(row[1]))
+                    {
+                        areas[row_segment_name].power_usage = parseInt(row[1]);
+                    }
+                }
+                // Extract camp information
+                if (row.length >= 2)
+                {
+                    // It's considored a camp if we have at least a name, column number 2
+                    if (row.length >= 2)
+                    {
+                        if (row[2]?.length > 0)
+                        {
+                            // Init camp
+                            var camp_name = row[2];
+                            areas[row_segment_name].camps[camp_name] = {};
+                            areas[row_segment_name].camps[camp_name].name = camp_name;
+                            areas[row_segment_name].camps[camp_name].projects = "";
+                            areas[row_segment_name].camps[camp_name].number_of_people = 0;
+                            areas[row_segment_name].camps[camp_name].number_of_vans = 0;
+                            areas[row_segment_name].camps[camp_name].other_structure_m2 = 0;
+                            areas[row_segment_name].camps[camp_name].power_usage = 0;
+                            areas[row_segment_name].camps[camp_name].contact_person = "";
+                            areas[row_segment_name].camps[camp_name].contact_contact = "";
+                            areas[row_segment_name].camps[camp_name].lnt_person = "";
+                            areas[row_segment_name].camps[camp_name].lnt_contact = "";
+                            areas[row_segment_name].camps[camp_name].consent_person = "";
+                            areas[row_segment_name].camps[camp_name].consent_contact = "";
+                            areas[row_segment_name].camps[camp_name].comment = "";
+
+                            for (let col_i = 0; col_i < row.length; col_i++)
+                            {
+                                if (col_i == 3)
+                                {
+                                    areas[row_segment_name].camps[camp_name].projects = row[col_i];
+                                }
+                                else if (col_i == 4)
+                                {
+                                    if (parseInt(row[col_i]))
+                                    {
+                                        areas[row_segment_name].camps[camp_name].number_of_people = parseInt(row[col_i]);
+                                    }
+                                }
+                                else if (col_i == 5)
+                                {
+                                    if (parseInt(row[col_i]))
+                                    {
+                                        areas[row_segment_name].camps[camp_name].number_of_vans = parseInt(row[col_i]);
+                                    }
+                                }
+                                else if (col_i == 6)
+                                {
+                                    if (parseInt(row[col_i]))
+                                    {
+                                        areas[row_segment_name].camps[camp_name].other_structure_m2 = parseInt(row[col_i]);
+                                    }
+                                }
+                                else if (col_i == 7)
+                                {
+                                    if (parseInt(row[col_i]))
+                                    {
+                                        areas[row_segment_name].camps[camp_name].power_usage = parseInt(row[col_i]);
+                                    }
+                                }
+                                else if (col_i == 8)
+                                {
+                                    areas[row_segment_name].camps[camp_name].contact_person = row[col_i];
+                                }
+                                else if (col_i == 9)
+                                {
+                                    areas[row_segment_name].camps[camp_name].contact_contact = row[col_i];
+                                }
+                                else if (col_i == 10)
+                                {
+                                    areas[row_segment_name].camps[camp_name].lnt_person = row[col_i];
+                                }
+                                else if (col_i == 11)
+                                {
+                                    areas[row_segment_name].camps[camp_name].lnt_contact = row[col_i];
+                                }
+                                else if (col_i == 12)
+                                {
+                                    areas[row_segment_name].camps[camp_name].consent_person = row[col_i];
+                                }
+                                else if (col_i == 13)
+                                {
+                                    areas[row_segment_name].camps[camp_name].consent_contact = row[col_i];
+                                }
+                                else if (col_i == 14)
+                                {
+                                    areas[row_segment_name].camps[camp_name].comment = row[col_i];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }    
+    }
+    // console.log(areas);
+    return areas;
+};
 
 export const loadCampClusters = async (map) => {
+    const areas_w_camps = await loadAreaCamps();
     const sheetdata = await loadSpreadSheet(CAMP_CLUSTERS_SPREADSHEET_JSON);
 
     const data = await loadGeoJson(CAMP_CLUSTERS_GEOJSON, () => ({
@@ -11,11 +188,11 @@ export const loadCampClusters = async (map) => {
             let color = '#03d7fc';
             let fillOpacity = 0.5;
 
-            //loop through sheetdata and add it to each feature
+            // Loop through sheetdata and add it to each feature
             for (let i = 0; i < sheetdata.length; i++) {
                 const [id, type, sheetname, maxarea, reservedarea, notice, description] = sheetdata[i];
 
-                // convert to string
+                // Convert to string
                 if (id === '' + feature.properties.fid) {
                     feature.properties = {
                         ...feature.properties,
@@ -33,12 +210,23 @@ export const loadCampClusters = async (map) => {
                     else if (type === 'bridge') color = 'yellow';
                     else if (type === 'building') color = 'brown';
 
-                    if (type === 'camp' || type === 'art' || type === 'building') {
-                        fillOpacity = 0;
-                        if (feature.properties.reservedarea > 500) {
+                    // If area ahs clusters loaded in areas_w_camps, check size-usage
+                    if (feature.properties.sheetname in areas_w_camps)
+                    {
+                        // console.log(feature.properties.sheetname + " is in areas_w_camps");
+                        feature.properties.camps = areas_w_camps[feature.properties.sheetname].camps;
+                        feature.properties.maxarea = areas_w_camps[feature.properties.sheetname].size;
+                        feature.properties.reservedarea = areas_w_camps[feature.properties.sheetname].size_reserved;
+                        feature.properties.description = areas_w_camps[feature.properties.sheetname].comment;
+                        feature.properties.size_usage_percent = areas_w_camps[feature.properties.sheetname].size_usage_percent;
+                        // If full, set color of area
+                        if (feature.properties.size_usage_percent > 100)
+                        {
                             color = 'red';
                             fillOpacity = 0.5;
-                        } else if (feature.properties.reservedarea > 0) {
+                        }
+                        else
+                        {
                             fillOpacity = (feature.properties.reservedarea / feature.properties.maxarea) * 0.75;
                         }
                     }
@@ -63,10 +251,9 @@ export const loadCampClusters = async (map) => {
             };
         },
         onEachFeature: (feature, layer) => {
+            let span = "<span style='color: white; text-shadow: 2px 2px #000000; font-weight: bold'>" + feature.properties.sheetname + '</span>';
             layer.bindTooltip(
-                "<span style='color: white; text-shadow: 2px 2px #000000; font-weight: bold'>" +
-                    feature.properties.sheetname +
-                    '</span>',
+                span,
                 { permanent: true, direction: 'center' },
             );
         },
@@ -87,6 +274,22 @@ export const loadCampClusters = async (map) => {
                 ' sqm.<BR>';
         }
 
+        let camps = '';
+        // console.log(layer.feature.properties.camps);
+        if (layer.feature.properties?.camps)
+        {
+            if (Object.keys(layer.feature.properties.camps).length > 0)
+            {
+                camps = '<h3>Camps in this cluster:</h3><ul class="camps-list">';
+                for (const [key, camp] of Object.entries(layer.feature.properties.camps))
+                {
+                    // console.log(key, camp);
+                    camps += "<li>" + camp.name + "</li>";
+                }
+                camps += "</ul>"
+            }
+        }
+
         let notice = '';
         if (layer.feature.properties.notice) notice = '<h3>' + layer.feature.properties.notice + '</h3>';
 
@@ -94,7 +297,7 @@ export const loadCampClusters = async (map) => {
         if (layer.feature.properties.description)
             description = '<B>Description:</B> ' + layer.feature.properties.description + '<BR>';
 
-        const content = '<h2>' + name + '</h2>' + area + notice + description;
+        const content = '<h2>' + name + '</h2>' + area + notice + description + camps;
 
         layer.bindPopup(content);
         layer.bringToFront();
