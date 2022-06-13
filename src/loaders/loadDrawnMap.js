5c0ac988d90bfb50d7ed04d38274ae5f8ddf5e86
Gusbod <skrapiskrap@gmail.com>
2022-06-13 18:39:37 +0200

    Added a drawn base map as a layer

---
+++ src/loaders/loadDrawnMap.js
@@ -0,0 +1,90 @@
+import { SOUNDGUIDE_GEOJSON } from '../constants';
+import L from 'leaflet';
+import { loadGeoJson } from '../utils/loadGeoJson';
+
+export const loadDrawnMap = async (map) => 
+{
+    map.createPane('drawnmap');
+    map.getPane('drawnmap').style.zIndex = 250;
+    map.getPane('drawnmap').style.pointerEvents = 'none';
+
+    let response = await fetch('./data/drawnmap/farmland.geojson');
+    let jsonData = await response.json();
+
+    const farmland = L.geoJson(jsonData, {
+                    style: 
+                    {
+                        color: '#fff7a6',
+                        fillColor: '#fff7a6',
+                        weight: 0,
+                        opacity: 1,
+                        fillOpacity: 1,
+                    }, 
+                    pane: 'drawnmap'});
+
+    response = await fetch('./data/drawnmap/forest.geojson');
+    jsonData = await response.json();
+
+    const forest = L.geoJson(jsonData, {
+                    style: 
+                    {
+                        color: '#acd47e',
+                        fillColor: '#acd47e',
+                        weight: 2,
+                        opacity: 1,
+                        fillOpacity: 1,
+                    }, 
+                    pane: 'drawnmap'});
+
+    response = await fetch('./data/drawnmap/water.geojson');
+    jsonData = await response.json();
+    
+    const water = L.geoJson(jsonData, {
+                    style: 
+                    {
+                    color: '#addfff',
+                    fillColor: '#addfff',
+                    weight: 2,
+                    opacity: 1,
+                    fillOpacity: 1,
+                    }, 
+                    pane: 'drawnmap'});
+
+    response = await fetch('./data/drawnmap/buildings.geojson');
+    jsonData = await response.json();
+    
+    const buildings = L.geoJson(jsonData, {
+                    style: 
+                    {
+                    color: 'darkgrey',
+                    fillColor: 'grey',
+                    weight: 2,
+                    opacity: 1,
+                    fillOpacity: 1,
+                    }, 
+                    pane: 'drawnmap'});
+
+    response = await fetch('./data/drawnmap/lakes.geojson');
+    jsonData = await response.json();
+    
+    const lakes = L.geoJson(jsonData, {
+                    style: 
+                    {
+                    color: '#addfff',
+                    fillColor: '#addfff',
+                    weight: 2,
+                    opacity: 1,
+                    fillOpacity: 1,
+                    }, 
+                    pane: 'drawnmap'});
+
+    let group = new L.FeatureGroup();
+    group.addLayer(farmland);
+    group.addLayer(forest);
+    group.addLayer(water);
+    group.addLayer(buildings);
+    group.addLayer(lakes);
+
+    return group;
+};
+
--- src/map.js
+++ src/map.js
@@ -11,31 +11,40 @@ import { loadTooltipZoom, loadBoarderlandMarker } from './utils/loadTooltipZoom'
 import { loadCampMarkers } from './utils/loadCampMarkers';
 import { loadPositionControl } from './utils/loadPositionControl';
 import { loadImageOverlay } from './loaders/loadImageOverlay';
+import { loadDrawnMap } from './loaders/loadDrawnMap';
 
 export const createMap = async () => {
     const map = L.map('map', { zoomControl: false, maxZoom: 21 }).setView([57.621111, 14.927857], 17);
 
-
-    loadZones(map);
-    loadNatureReserve(map);
-    loadFireRoads(map);
-    loadTooltipZoom(map);
-    loadBoarderlandMarker(map);
-    loadPositionControl(map);
+    await loadZones(map);
+    await loadNatureReserve(map);
+    await loadFireRoads(map);
+    await loadTooltipZoom(map);
+    await loadBoarderlandMarker(map);
+    await loadPositionControl(map);
     await loadCampClusters(map);
     await loadCampMarkers(map);
-    let sound = await loadSoundGuide(map);
-    let slopemap = await loadImageOverlay(map, './data/slopemap.png', [[57.6183258637506626, 14.9211877664388641], [57.6225237073944072,14.9346879887464876]]);
-    let terrain = await loadImageOverlay(map, './data/terrain.png', [[57.6156422900704257, 14.9150971736724536], [57.6291230394961715,14.9362178462290363]]);
+
     L.control.scale({metric: true, imperial: false}).addTo(map);
     L.control.polylineMeasure().addTo(map);
     let hash = new L.Hash(map);  // Makes THE URL follow the map
 
-    var baseLayers = {"Sound guide": sound, "Terrain": terrain, "Slope map": slopemap};
-    L.control.layers(null, baseLayers).addTo(map);
-
+    //Add sound guide, slope map and terrain map to layer control
+    let sound = await loadSoundGuide(map);
+    let slopemap = await loadImageOverlay(map, './data/slopemap.png', [[57.6183258637506626, 14.9211877664388641], [57.6225237073944072,14.9346879887464876]]);
+    let terrain = await loadImageOverlay(map, './data/terrain.png', [[57.6156422900704257, 14.9150971736724536], [57.6291230394961715,14.9362178462290363]]);
+    var extraLayers = {"Sound guide": sound, "Terrain": terrain, "Slope map": slopemap};
+    
+    
+    //Base layers
     const googleSatellite = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
     }).addTo(map);
+    
+    let drawnmap = await loadDrawnMap(map);
+
+    var baseLayers = {"Satellite map": googleSatellite, "Drawn map": drawnmap};
+
+    L.control.layers(baseLayers, extraLayers).addTo(map);
 };
