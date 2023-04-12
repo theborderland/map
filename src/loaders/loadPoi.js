69099b15bbde51cafcee7ff7e4e00d16da9c476f
Lyret <viktor@lyresten.se>
2023-04-12 22:46:56 +0200

    fixed editor warnings in older file

--- src/loaders/loadPoi.js
+++ src/loaders/loadPoi.js
@@ -1,38 +1,36 @@
 import { loadGoogleSpreadSheet } from '../utils/loadSpreadSheet';
 import { PLACEMENT_MAP_SHEET } from '../constants';
-import L, { polyline } from 'leaflet';
+import L from 'leaflet';
 
 const iconsSize = 48;
 const iconAnchor = iconsSize * 0.5;
 
 var centeredIcon = L.Icon.extend({
     options: {
-        iconSize:     [iconsSize, iconsSize],
-        iconAnchor:   [iconAnchor,iconAnchor],
-        popupAnchor:  [0,-iconsSize*0.25]
-    }
+        iconSize: [iconsSize, iconsSize],
+        iconAnchor: [iconAnchor, iconAnchor],
+        popupAnchor: [0, -iconsSize * 0.25],
+    },
 });
 
-export const loadPoi = async (map) => 
-{
+export const loadPoi = async () => {
     const spreadsheetdata = await loadGoogleSpreadSheet(PLACEMENT_MAP_SHEET, 'poi!A2:F');
     let iconDict = {};
 
     let poiLayer = L.layerGroup();
 
-    for (let i = 0; i < spreadsheetdata.length; i++) 
-    {
-        if (spreadsheetdata[i][0]) //Check if 'type' column is not empty
-        {
-            const [type,name,description,lonlat] = spreadsheetdata[i];
-            const [lon, lat] = lonlat.split(",");
+    for (let i = 0; i < spreadsheetdata.length; i++) {
+        if (spreadsheetdata[i][0]) {
+            //Check if 'type' column is not empty
+            const [type, name, description, lonlat] = spreadsheetdata[i];
+            const [lon, lat] = lonlat.split(',');
 
-            if (!iconDict[type]) iconDict[type] = new centeredIcon({iconUrl: './img/icons/' + type + '.png'});
-            
+            if (!iconDict[type]) iconDict[type] = new centeredIcon({ iconUrl: './img/icons/' + type + '.png' });
+
             let navigatehere = ' ';
             navigatehere += '<a';
             navigatehere += ' href="';
-            navigatehere += 'https://tim.gremalm.se/gps/updategps.php?lat='
+            navigatehere += 'https://tim.gremalm.se/gps/updategps.php?lat=';
             navigatehere += lon;
             navigatehere += '&lng=';
             navigatehere += lat;
@@ -41,11 +39,9 @@ export const loadPoi = async (map) =>
             navigatehere += '>';
             navigatehere += '☩';
             navigatehere += '</a>';
-    
+
             const content = '<h3>' + name + '</h3>' + '<p>' + description + navigatehere + '</p>';
-            L.marker([lon, lat],
-                {icon: iconDict[type]
-                }).addTo(poiLayer).bindPopup(content);
+            L.marker([lon, lat], { icon: iconDict[type] }).addTo(poiLayer).bindPopup(content);
         }
     }
 
