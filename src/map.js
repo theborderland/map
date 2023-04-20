7453461234743ead6cb8cf5cdb0322b3b7d7b45c
Gusbod <skrapiskrap@gmail.com>
2023-04-20 08:43:34 +0200

    Added slope and height tile layers

--- src/map.js
+++ src/map.js
@@ -43,22 +43,36 @@ export const createMap = async () => {
     var baseLayers = { 'Satellite map': map.groups.googleSatellite, 'Drawn map': map.groups.drawnmap };
 
     // Extra layers
-    map.groups.slopemap = await loadImageOverlay(map, './data/slopemap.png', [
-        [57.6183258637506626, 14.9211877664388641],
-        [57.6225237073944072, 14.9346879887464876],
-    ]);
+
     map.groups.terrain = await loadImageOverlay(map, './data/terrain.png', [
         [57.6156422900704257, 14.9150971736724536],
         [57.6291230394961715, 14.9362178462290363],
     ]);
 
+    map.groups.slopemap = L.tileLayer('./data/analysis/slope/{z}/{x}/{y}.png', {
+        minZoom: 13,
+        maxZoom: 21,
+        minNativeZoom: 16,
+        maxNativeZoom: 17,
+        tms: false
+      });
+
+    map.groups.heightmap = L.tileLayer('./data/analysis/height/{z}/{x}/{y}.jpg', {
+        minZoom: 13,
+        maxZoom: 21,
+        minNativeZoom: 16,
+        maxNativeZoom: 17,
+        tms: false
+      });
+      
     // map.groups.poi_menu = (new L.LayerGroup()).addTo(map);
     // map.groups.power_menu = (new L.LayerGroup());
 
     var extraLayers = {
         Placement: map.groups.placement,
+        Slope: map.groups.slopemap,
+        Height: map.groups.heightmap,
         Terrain: map.groups.terrain,
-        'Slope map': map.groups.slopemap,
         // "Power": map.groups.power_menu,
     };
 
