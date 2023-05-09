9af59e47a910af4eb3c889f57e6e2668a6a005ac
myding <johndoe@example.com>
2023-05-10 00:17:04 +0200

    Add comments

--- src/loaders/loadGeoJsonFeatureCollections.js
+++ src/loaders/loadGeoJsonFeatureCollections.js
@@ -1,5 +1,10 @@
 import L from 'leaflet';
-
+// This class is used to load GeoJSON data and create a layer per unique value of a property. Used for example by the placement
+// layer that has different "type" properties that can easily go into different layers.
+// groupByProperty is the property to group by
+// styleFunction is a function that returns the style for a feature, so that different styles can be applied to different groups
+// filename is the name of the file to load
+// map is the map to add the layers to
 export const loadGeoJsonFeatureCollections = async (map, styleFunction, groupByProperty, filename) => {
   const response = await fetch(filename);
   const geojsonData = await response.json();
