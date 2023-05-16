666c991b6e500140878c4cebdc66693d120d3a6c
Gusbod <skrapiskrap@gmail.com>
2023-05-16 12:58:22 +0200

    Deactivate location control temporarily

--- src/map.js
+++ src/map.js
@@ -137,7 +137,7 @@ export const createMap = async () => {
     // L.control.scale({ metric: true, imperial: false, position: 'bottomright' }).addTo(map);
     
     // Reactivate closer to BL. Double check this functionality, reportedly buggy.
-    L.control.locate({ setView: 'once', keepCurrentZoomLevel: true,	returnToPrevBounds: true, drawCircle: true,	flyTo: true}).addTo(map);
+    // L.control.locate({ setView: 'once', keepCurrentZoomLevel: true,	returnToPrevBounds: true, drawCircle: true,	flyTo: true}).addTo(map);
     
     let polylineMeasure = L.control.polylineMeasure({measureControlLabel: '&#128207;', arrow: {color: '#0000',} });
     polylineMeasure.addTo(map);
