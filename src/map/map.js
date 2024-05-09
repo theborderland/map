eaea5e6b70143584895edf9d5436143bd780b868
Lyret <viktor@lyresten.se>
2024-05-09 14:38:06 +0200

    refactors public folder

--- src/map/map.js
+++ src/map/map.js
@@ -23,7 +23,7 @@ function encodeHashMeta(layers) {
     const hashMeta = [];
 
     if (layers.length > 0) {
-        hashMeta.push('layers:' + layers.join(','))
+        hashMeta.push('layers:' + layers.join(','));
     }
 
     return hashMeta;
@@ -31,7 +31,7 @@ function encodeHashMeta(layers) {
 
 function decodeHashMeta(hashMeta) {
     const layersKey = 'layers:';
-    const layersVal = hashMeta.find(val => val.startsWith(layersKey));
+    const layersVal = hashMeta.find((val) => val.startsWith(layersKey));
 
     const layers = layersVal ? layersVal.substring(layersKey.length).split(',') : [];
 
@@ -40,10 +40,12 @@ function decodeHashMeta(hashMeta) {
 
 export const createMap = async () => {
     // default visible map layers
-    let visibleLayers = new Set(['Placement', 'Placement_map']);
+    let visibleLayers = new Set(['Placement_map']); // FIXME: re-add placement?
 
-    const map = L.map('map', { zoomControl: false, maxZoom: 21, drawControl: true, attributionControl: false })
-    .setView([57.6226, 14.9276], 16);
+    const map = L.map('map', { zoomControl: false, maxZoom: 21, drawControl: true, attributionControl: false }).setView(
+        [57.6226, 14.9276],
+        16,
+    );
 
     map.groups = {};
 
@@ -56,54 +58,61 @@ export const createMap = async () => {
     await loadPoiFromGoogleCsv(map);
 
     //Only show message if user has not seen instructions yet
-    if (localStorage.getItem('hasSeenPlacementInstructions') === null)
-    {
+    if (localStorage.getItem('hasSeenPlacementInstructions') === null) {
         showBetaMsg();
     }
 
     map.groups.mapstuff = new L.LayerGroup();
 
     //Load placenames
-    fetch('./data/bl23/placenames.geojson').then(response => response.json()).then(response => {
-      L.geoJSON(response.features, {style: {"color": "#ffffff", "weight": 2}}).addTo(map.groups.mapstuff);
-    });
+    // fetch('./data/bl23/placenames.geojson')
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //         L.geoJSON(response.features, { style: { color: '#ffffff', weight: 2 } }).addTo(map.groups.mapstuff);
+    //     });
 
     //Load contours
-    fetch('./data/analysis/contours.geojson').then(response => response.json()).then(response => {
-      L.geoJSON(response.features, {style: {"color": "#ffffff", "weight": 1, "opacity": 0.5}}).addTo(map.groups.mapstuff);
-    });
-    
+    // fetch('./data/analysis/contours.geojson')
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //         L.geoJSON(response.features, { style: { color: '#ffffff', weight: 1, opacity: 0.5 } }).addTo(
+    //             map.groups.mapstuff,
+    //         );
+    //     });
+
     //Load reference drawings
-    fetch('./data/bl23/references.geojson').then(response => response.json()).then(response => {
-      L.geoJSON(response.features, {style: {"color": "#ffffff", "weight": 1}}).addTo(map.groups.mapstuff);
-    });
+    // fetch('./data/bl23/references.geojson')
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //         L.geoJSON(response.features, { style: { color: '#ffffff', weight: 1 } }).addTo(map.groups.mapstuff);
+    //     });
 
-    await loadGeoJsonFeatureCollections(map, getStyleFunction, 'type', './data/bl23/borders.geojson');
+    await loadGeoJsonFeatureCollections(map, getStyleFunction, 'type', './data/bl24/fire.geojson');
     await loadGeoJsonFeatureCollections(map, getStyleFunction, 'type', './data/bl23/placement.geojson');
-    
-    //Set up the soundguide layers added from placement.geojson
-    map.groups.soundguide = new L.LayerGroup();
-    map.groups.soundhigh.addTo(map.groups.soundguide);
-    map.groups.soundmedium.addTo(map.groups.soundguide);
-    map.groups.soundmediumlow.addTo(map.groups.soundguide);
-    map.groups.soundlow.addTo(map.groups.soundguide);
-    map.groups.soundquiet.addTo(map.groups.soundguide);
-    map.removeLayer(map.groups.soundhigh);
-    map.removeLayer(map.groups.soundmedium);
-    map.removeLayer(map.groups.soundmediumlow);
-    map.removeLayer(map.groups.soundlow);
-    map.removeLayer(map.groups.soundquiet);
 
-    map.groups.fireroad.addTo(map.groups.mapstuff);
-    map.groups.propertyborder.addTo(map.groups.mapstuff);
-    map.groups.highprio.addTo(map.groups.mapstuff);
-    map.groups.lowprio.addTo(map.groups.mapstuff);
-    map.groups.hiddenforbidden.addTo(map.groups.mapstuff);
-    map.groups.container.addTo(map.groups.mapstuff);
-    map.groups.parking.addTo(map.groups.mapstuff);
-    map.groups.toilet.addTo(map.groups.mapstuff);
-    map.groups.bridge.addTo(map.groups.mapstuff);
-
+    //     //Set up the soundguide layers added from placement.geojson
+    //     map.groups.soundguide = new L.LayerGroup();
+    //     map.groups.soundhigh.addTo(map.groups.soundguide);
+    //     map.groups.soundmedium.addTo(map.groups.soundguide);
+    //     map.groups.soundmediumlow.addTo(map.groups.soundguide);
+    //     map.groups.soundlow.addTo(map.groups.soundguide);
+    //     map.groups.soundquiet.addTo(map.groups.soundguide);
+    //     map.removeLayer(map.groups.soundhigh);
+    //     map.removeLayer(map.groups.soundmedium);
+    //     map.removeLayer(map.groups.soundmediumlow);
+    //     map.removeLayer(map.groups.soundlow);
+    //     map.removeLayer(map.groups.soundquiet);
+    //
+    //     console.log(Object.keys(map.groups));
+    //     map.groups.fireroad.addTo(map.groups.mapstuff);
+    //     map.groups.propertyborder.addTo(map.groups.mapstuff);
+    //     map.groups.highprio.addTo(map.groups.mapstuff);
+    //     map.groups.lowprio.addTo(map.groups.mapstuff);
+    //     map.groups.hiddenforbidden.addTo(map.groups.mapstuff);
+    //     map.groups.container.addTo(map.groups.mapstuff);
+    //     map.groups.parking.addTo(map.groups.mapstuff);
+    //     map.groups.toilet.addTo(map.groups.mapstuff);
+    //     map.groups.bridge.addTo(map.groups.mapstuff);
 
     //Initialize the editor (it loads it data at the end)
     const editor = new Editor(map, map.groups);
@@ -119,7 +128,7 @@ export const createMap = async () => {
         maxZoom: 21,
         minNativeZoom: 16,
         maxNativeZoom: 17,
-        tms: false
+        tms: false,
     });
 
     map.groups.slopemap = L.tileLayer('./data/analysis/slope/{z}/{x}/{y}.png', {
@@ -127,7 +136,7 @@ export const createMap = async () => {
         maxZoom: 21,
         minNativeZoom: 16,
         maxNativeZoom: 17,
-        tms: false
+        tms: false,
     });
 
     map.groups.aftermath = L.tileLayer('./data/bl23/aftermath/{z}/{x}/{y}.png', {
@@ -135,7 +144,7 @@ export const createMap = async () => {
         maxZoom: 21,
         minNativeZoom: 0,
         maxNativeZoom: 20,
-        tms: false
+        tms: false,
     });
 
     map.groups.aftermath22 = L.tileLayer('./data/bl22/aftermath/{z}/{x}/{y}.png', {
@@ -143,7 +152,7 @@ export const createMap = async () => {
         maxZoom: 21,
         minNativeZoom: 0,
         maxNativeZoom: 20,
-        tms: false
+        tms: false,
     });
 
     map.groups.power = new L.LayerGroup();
@@ -168,23 +177,20 @@ export const createMap = async () => {
         Check_Clean: map.groups.clean,
     };
 
-    map.on("hashmetainit", function(initState) {
+    map.on('hashmetainit', function (initState) {
         const { layers } = decodeHashMeta(initState.meta);
 
-        layers
-            .filter(name => name in extraLayers)
-            .forEach(layerName => visibleLayers.add(layerName));
+        layers.filter((name) => name in extraLayers).forEach((layerName) => visibleLayers.add(layerName));
 
-        visibleLayers.forEach(layer => map.addLayer(extraLayers[layer]))
+        visibleLayers.forEach((layer) => map.addLayer(extraLayers[layer]));
     });
 
-    const hash = new L.Hash(map);  // Makes the URL follow the map.
+    const hash = new L.Hash(map); // Makes the URL follow the map.
 
     // force update of URL hash on first load
     hash.setHashMeta(encodeHashMeta(visibleLayers), true);
 
-    map.on('overlayadd', function (eventLayer)
-    {
+    map.on('overlayadd', function (eventLayer) {
         if (eventLayer.name === 'Check_Power') editor.setLayerFilter('power', false);
         else if (eventLayer.name === 'Check_Sound') editor.setLayerFilter('sound', false);
         else if (eventLayer.name === 'Check_Clean') editor.setLayerFilter('cleancolors', false);
@@ -198,13 +204,13 @@ export const createMap = async () => {
         }
     });
 
-    map.on('overlayremove', function (eventLayer) 
-    {
-        if (eventLayer.name === 'Check_Power' || eventLayer.name === 'Check_Sound' || eventLayer.name === 'Check_Clean') editor.setLayerFilter('severity', false);
+    map.on('overlayremove', function (eventLayer) {
+        if (eventLayer.name === 'Check_Power' || eventLayer.name === 'Check_Sound' || eventLayer.name === 'Check_Clean')
+            editor.setLayerFilter('severity', false);
     });
 
     //log the lat and long to the console when clicking the map or a layer or marker
-    map.on('click', function(e) {
+    map.on('click', function (e) {
         console.log(e.latlng);
     });
 
@@ -227,22 +233,22 @@ export const createMap = async () => {
 
     // Add map features
     // L.control.scale({ metric: true, imperial: false, position: 'bottomright' }).addTo(map);
-    
+
     // Reactivate closer to BL. Double check this functionality, reportedly buggy.
     // L.control.locate({ setView: 'once', keepCurrentZoomLevel: true,	returnToPrevBounds: true, drawCircle: true,	flyTo: true}).addTo(map);
-    
-    let polylineMeasure = L.control.polylineMeasure({measureControlLabel: '&#128207;', arrow: {color: '#0000',} });
-    polylineMeasure.addTo(map);
-   
-    //Load all entities from the API
-    await editor.addAPIEntities();
-    
-    //Access the query string and zoom to entity if id is present
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    if (id) {
-        editor.gotoEntity(id);
-    }
 
-    editor.loadingScreenDescription('Entities is load, now evaluate them.');
+    let polylineMeasure = L.control.polylineMeasure({ measureControlLabel: '&#128207;', arrow: { color: '#0000' } });
+    polylineMeasure.addTo(map);
+
+    //Load all entities from the API
+    //await editor.addAPIEntities();
+
+    // //Access the query string and zoom to entity if id is present
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const id = urlParams.get('id');
+    // if (id) {
+    //     editor.gotoEntity(id);
+    // }
+
+    //editor.loadingScreenDescription('Entities is load, now evaluate them.');
 };
