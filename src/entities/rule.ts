4a02c2f9cd02cdaa258b3ae4c1cc0ddf708ebb32
Sigurd <sigurdcarlsen@gmail.com>
2024-05-10 16:21:12 +0200

    Fix not catching polygons contained in eachother

--- src/entities/rule.ts
+++ src/entities/rule.ts
@@ -401,7 +401,7 @@ function _getTotalAreaOfOverlappingEntities(layer: L.Layer, layerGroup: L.LayerG
 
                         //@ts-ignore
                         let buffer = Turf.buffer(layer.toGeoJSON(), FIRE_BUFFER_IN_METER, { units: 'meters' }) as Turf.helpers.FeatureCollection<Turf.helpers.Polygon>;
-                        if (Turf.booleanOverlap(buffer.features[0], otherLayerPolygon)) {
+                        if (Turf.booleanOverlap(buffer.features[0], otherLayerPolygon)||Turf.booleanContains(buffer.features[0], otherLayerPolygon)) {
                             overlaps=true
                         }else{
                             overlaps = false
