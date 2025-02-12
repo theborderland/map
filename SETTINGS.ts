18b2f2fcddae5d3874bc8c9662d361d58bbdd2a7
Robin <bobinush@gmail.com>
2025-02-12 16:35:08 +0100

    Remove editing flag
    Co-authored-by: Viktor Lyresten <lyret@users.noreply.github.com>

--- SETTINGS.ts
+++ SETTINGS.ts
@@ -3,7 +3,7 @@
  */
 
 // Editor
-export const IS_EDITING_POSSIBLE: boolean = true;
+export const IS_EDITING_POSSIBLE: boolean = false;
 export const NOTE_ABOUT_EDITING: string = undefined; // 'THE MAP IS RESETTING TUESDAY 10.00 AM CET';
 
 // Repository
