b3e0d93beb7aca13e5c739819df7f0da5615234d
Robin Nowakowski <bobinush@gmail.com>
2025-02-12 16:52:33 +0100

    remove local dev URL

--- SETTINGS.ts
+++ SETTINGS.ts
@@ -7,7 +7,7 @@ export const IS_EDITING_POSSIBLE: boolean = false;
 export const NOTE_ABOUT_EDITING: string = undefined; // 'THE MAP IS RESETTING TUESDAY 10.00 AM CET';
 
 // Repository
-export const REPOSITORY_URL: string = 'https://localhost:7076'; // https://placement.freaks.se
+export const REPOSITORY_URL: string = 'https://placement.freaks.se';
 
 // Rules
 export const MAX_CLUSTER_SIZE: number = 1250;
