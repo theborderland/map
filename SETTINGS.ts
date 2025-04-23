fa44618c22021abf4a99a89186785e6633397e4a
Robin Nowakowski <bobinush@gmail.com>
2025-04-23 21:55:09 +0800

    Start the test period!

--- SETTINGS.ts
+++ SETTINGS.ts
@@ -3,11 +3,13 @@
  */
 
 // Editor
-export const IS_EDITING_POSSIBLE: boolean = false;
-export const NOTE_ABOUT_EDITING: string = undefined; // 'THE MAP IS RESETTING TUESDAY 10.00 AM CET';
+export const IS_EDITING_POSSIBLE: boolean = true;
+export const NOTE_ABOUT_EDITING: string = `MAP IS IN TEST MODE!
+<img style='max-width:100%;' src='/img/test-in-production.jpg' />
+Test data is from 2024 <br>Test period ends May 4th`; // 'THE MAP IS RESETTING TUESDAY 10.00 AM CET';
 
 // Repository
-export const REPOSITORY_URL: string = 'https://placement.freaks.se';
+export const REPOSITORY_URL: string = 'https://robnowa.runasp.net';
 
 // Rules
 export const MAX_CLUSTER_SIZE: number = 1250;
