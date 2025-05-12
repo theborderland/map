0de79417a9a13580ca880c718d658ebc76dbd65a
Jimmy Ghaderi (jigh01) <jimmy.ghaderi@svt.se>
2025-05-12 20:54:45 +0200

    handle issues with rule checking, log warnings

--- src/rule/rules/isOverlappingOrContained.ts
+++ src/rule/rules/isOverlappingOrContained.ts
@@ -1,36 +1,48 @@
 import * as Turf from '@turf/turf';
 import { Severity, Rule } from '../index';
 
-export const isOverlappingOrContained = (
-    layerGroup: any, 
-    severity: Severity, 
-    shortMsg: string, 
-    message: string
-) => new Rule(severity, shortMsg, message, (entity) => {
-    let geoJson = entity.toGeoJSON();
-    let overlap = false;
+export const isOverlappingOrContained = (layerGroup: any, severity: Severity, shortMsg: string, message: string) =>
+    new Rule(severity, shortMsg, message, (entity, ruleMessage) => {
+        let geoJson = entity.toGeoJSON();
+        let overlap = false;
 
-    layerGroup.eachLayer((layer) => {
-        //@ts-ignore
-        let otherGeoJson = layer.toGeoJSON();
+        if (!layerGroup) {
+            console.warn('No layer group provided in isOverlappingOrContained:', { ruleMessage, entity });
+            return { triggered: false };
+        }
 
-        //Loop through all features if it is a feature collection
-        if (otherGeoJson.features) {
-            for (let i = 0; i < otherGeoJson.features.length; i++) {
-                if (Turf.booleanOverlap(geoJson, otherGeoJson.features[i]) ||
-                    Turf.booleanContains(otherGeoJson.features[i], geoJson)) {
-                    overlap = true;
-                    return; // Break out of the inner loop
+        layerGroup.eachLayer((layer) => {
+            //@ts-ignore
+            let otherGeoJson = layer.toGeoJSON();
+
+            //Loop through all features if it is a feature collection
+            if (otherGeoJson.features) {
+                for (let i = 0; i < otherGeoJson.features.length; i++) {
+                    if (
+                        Turf.booleanOverlap(geoJson, otherGeoJson.features[i]) ||
+                        Turf.booleanContains(otherGeoJson.features[i], geoJson)
+                    ) {
+                        overlap = true;
+                        return; // Break out of the inner loop
+                    }
                 }
+            } else if (geoJson.geometry.type === otherGeoJson.geometry.type) {
+                if (Turf.booleanOverlap(geoJson, otherGeoJson) || Turf.booleanContains(otherGeoJson, geoJson)) {
+                    overlap = true;
+                    return; // Break out of the loop once an overlap is found
+                }
+            } else {
+                console.warn('Mismatched types in isOverlappingOrContained:', {
+                    ruleMessage,
+                    geoJson,
+                    otherGeoJson,
+                });
             }
-        } else if (Turf.booleanOverlap(geoJson, otherGeoJson) || Turf.booleanContains(otherGeoJson, geoJson)) {
-            overlap = true;
-        }
 
-        if (overlap) {
-            return; // Break out of the loop once an overlap is found
-        }
+            if (overlap) {
+                return; // Break out of the loop once an overlap is found
+            }
+        });
+
+        return { triggered: overlap };
     });
-
-    return { triggered: overlap };
-});
