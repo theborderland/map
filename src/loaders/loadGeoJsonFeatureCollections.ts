d2c22cc20255128968a6395fcbb666398d292604
Robin Nowakowski <bobinush@gmail.com>
2025-05-12 17:28:19 +0200

    Add fire and minor roads (with new buffer function), add plazas

--- src/loaders/loadGeoJsonFeatureCollections.ts
+++ src/loaders/loadGeoJsonFeatureCollections.ts
@@ -1,5 +1,6 @@
 import L from 'leaflet';
 import { getStyleFunction } from './_layerStyles';
+import * as Turf from '@turf/turf';
 
 // This class is used to load GeoJSON data and create a layer per unique value of a property. Used for example by the placement
 // layer that has different "type" properties that can easily go into different layers.
@@ -7,11 +8,23 @@ import { getStyleFunction } from './_layerStyles';
 // styleFunction is a function that returns the style for a feature, so that different styles can be applied to different groups
 // filename is the name of the file to load
 // map is the map to add the layers to
-export const loadGeoJsonFeatureCollections = async (map, groupByProperty, filename) => {
+export const loadGeoJsonFeatureCollections = async (map, groupByProperty, filename, buffer = 0) => {
     const response = await fetch(filename);
     const geojsonData = await response.json();
 
-    // Find unique names
+    // Add optional buffer to the features
+    if (buffer > 0) {
+        for (let i = 0; i < geojsonData.features.length; i++) {
+            if (geojsonData.features[i].geometry.coordinates.length === 0) {
+                continue; // needs to have coordinates
+            }
+            // Add a buffer to the feature
+            geojsonData.features[i] = Turf.buffer(geojsonData.features[i], buffer, {
+                units: 'meters'
+            });
+        }
+    }
+
     const uniqueNames = new Set<string>(geojsonData.features.map((feature) => feature.properties[groupByProperty]));
 
     //Create groups per type
@@ -20,7 +33,7 @@ export const loadGeoJsonFeatureCollections = async (map, groupByProperty, filena
             filter: filterByProperty(groupByProperty, value),
             style: getStyleFunction(value),
         });
-
+        
         map.groups[value] = geojsonLayer;
     });
 };
