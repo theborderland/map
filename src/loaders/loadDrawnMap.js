b3dfbd7c6e7ba01a92dce8a43cd4fb5975626c9e
Robin Nowakowski <bobinush@gmail.com>
2025-06-12 17:12:51 +0200

    Refactor loadDrawnMap, add new drawn map layout

--- src/loaders/loadDrawnMap.js
+++ src/loaders/loadDrawnMap.js
@@ -1,88 +1,47 @@
 import L from 'leaflet';
+/*
+USAGE: 
+await loadDrawnMap(map);
+map.addLayer(map.groups.drawnmap);
+*/
 
-export const loadDrawnMap = async (map) => 
-{
+let getStyle = (color) => {
+    return {
+        stroke: false,
+        fillColor: color,
+        weight: 0,
+        opacity: 1,
+        fillOpacity: 1,
+    };
+};
+const geoJsonFiles = [
+    { url: './data/drawnmap/roads.geojson', color: '#b3bcc3' },
+    { url: './data/drawnmap/farmland.geojson', color: '#8fc981' },
+    { url: './data/drawnmap/grass.geojson', color: '#8fc981' },
+    { url: './data/drawnmap/forest.geojson', color: '#27763d' },
+    { url: './data/drawnmap/buildings.geojson', color: '#fef2cb' },
+    { url: './data/drawnmap/lakes.geojson', color: '#8fdaed' },
+    { url: './data/drawnmap/water.geojson', color: '#8fdaed' },
+];
+
+export const loadDrawnMap = async (map) => {
     map.createPane('drawnmap');
     map.getPane('drawnmap').style.zIndex = 250;
     map.getPane('drawnmap').style.pointerEvents = 'none';
 
-    let response = await fetch('./data/drawnmap/farmland.geojson');
-    let jsonData = await response.json();
-
-    const farmland = L.geoJson(jsonData, {
-                    style: 
-                    {
-                        color: '#fff7a6',
-                        fillColor: '#fff7a6',
-                        weight: 0,
-                        opacity: 1,
-                        fillOpacity: 1,
-                    }, 
-                    pane: 'drawnmap'});
-
-    response = await fetch('./data/drawnmap/forest.geojson');
-    jsonData = await response.json();
-
-    const forest = L.geoJson(jsonData, {
-                    style: 
-                    {
-                        color: '#acd47e',
-                        fillColor: '#acd47e',
-                        weight: 2,
-                        opacity: 1,
-                        fillOpacity: 1,
-                    }, 
-                    pane: 'drawnmap'});
-
-    response = await fetch('./data/drawnmap/water.geojson');
-    jsonData = await response.json();
-    
-    const water = L.geoJson(jsonData, {
-                    style: 
-                    {
-                    color: '#addfff',
-                    fillColor: '#addfff',
-                    weight: 2,
-                    opacity: 1,
-                    fillOpacity: 1,
-                    }, 
-                    pane: 'drawnmap'});
-
-    response = await fetch('./data/drawnmap/buildings.geojson');
-    jsonData = await response.json();
-    
-    const buildings = L.geoJson(jsonData, {
-                    style: 
-                    {
-                    color: 'darkgrey',
-                    fillColor: 'grey',
-                    weight: 2,
-                    opacity: 1,
-                    fillOpacity: 1,
-                    }, 
-                    pane: 'drawnmap'});
-
-    response = await fetch('./data/drawnmap/lakes.geojson');
-    jsonData = await response.json();
-    
-    const lakes = L.geoJson(jsonData, {
-                    style: 
-                    {
-                    color: '#addfff',
-                    fillColor: '#addfff',
-                    weight: 2,
-                    opacity: 1,
-                    fillOpacity: 1,
-                    }, 
-                    pane: 'drawnmap'});
+    const layers = [];
+    for (const { url, color } of geoJsonFiles) {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        const layer = L.geoJson(jsonData, {
+            style: getStyle(color),
+            pane: 'drawnmap'
+        });
+        layers.push(layer);
+    }
 
     let group = new L.FeatureGroup();
-    group.addLayer(farmland);
-    group.addLayer(forest);
-    group.addLayer(water);
-    group.addLayer(buildings);
-    group.addLayer(lakes);
-
+    layers.forEach(layer => group.addLayer(layer));
     map.groups.drawnmap = group;
 };
 
