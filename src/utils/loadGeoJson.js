4e1902b158c726f3f356863ce43d8fb1ebae4f57
Robin Nowakowski <bobinush@gmail.com>
2025-08-03 18:15:55 +0200

    Cleanup - remove unused files

--- src/utils/loadGeoJson.js
+++
@@ -1,9 +0,0 @@
-import L from 'leaflet';
-
-export const loadGeoJson = async(url, options) => {
-    const response = await fetch(url)
-    const jsonData = await response.json()
-
-    return L.geoJson(jsonData, options(jsonData))
-}
-
