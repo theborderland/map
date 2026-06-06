070441f50ead68d54bba054b9a0f377245218906
Robin Nowakowski <bobinush@gmail.com>
2026-06-06 14:38:02 +0200

    downgrade vite

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/map/admin/",
+  base: "/admin/",
   server: {
     proxy: {
       // everything NOT /admin goes to map
