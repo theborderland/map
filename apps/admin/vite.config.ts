70fd05bb2b6cbcc4d56d3309d13fbfe9d1635d5e
Robin Nowakowski <bobinush@gmail.com>
2026-06-06 21:29:55 +0200

    fix vite mapping in prod

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,29 +2,34 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/admin/",
-  server: {
-    proxy: {
-      // everything NOT /admin goes to map
-      "^/(?!admin)": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    plugins: [react()],
+
+    base: isProd ? "/map/admin/" : "/admin/",
+    server: {
+      proxy: {
+        // everything NOT /admin goes to map
+        "^/(?!admin)": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    outDir: "../../public/admin", // IMPORTANT
-    emptyOutDir: true,
-    target: "es2020",
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
+    build: {
+      outDir: "../../public/admin", // IMPORTANT
+      emptyOutDir: true,
+      target: "es2020",
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name].js",
+          assetFileNames: "[name].[ext]",
+        },
       },
-    },
-  },
-  
+    }
+  }
+
 });
